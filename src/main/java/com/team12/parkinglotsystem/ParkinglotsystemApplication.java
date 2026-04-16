ed4f88fc9fd5c4c6b39ff53b4e3815fd483a02d5
RamPrakhyath05 <ram.annamreddy25@gmail.com>
2026-04-16 12:42:54 +0530

    Initial Commit: create spring boot application

---
+++ src/main/java/com/team12/parkinglotsystem/ParkinglotsystemApplication.java
@@ -0,0 +1,13 @@
+package com.team12.parkinglotsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinglotsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team12/parkinglotsystem/ParkinglotsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team12.parkinglotsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
