b7af041688421ef1e3f5926e316a08bba0c86a16
Rahul6700 <rahulsk4087@gmail.com>
2026-04-19 22:25:51 +0530

    feat: ticket class extension

--- src/main/java/com/team12/parkinglotsystem/model/Ticket.java
+++ src/main/java/com/team12/parkinglotsystem/model/Ticket.java
@@ -2,6 +2,7 @@ package com.team12.parkinglotsystem.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tickets")
@@ -10,17 +11,39 @@ import lombok.*;
 @AllArgsConstructor
 public class Ticket {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @ManyToOne
-  @JoinColumn(name = "slot_number", nullable = false)
-  private ParkingSlot slot;
+    @ManyToOne
+    @JoinColumn(name = "slot_number", nullable = false)
+    private ParkingSlot slot;
 
-  @ManyToOne
-  @JoinColumn(name = "vehicle_id", nullable = false)
-  private Vehicle vehicle;
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
 
-  // Member B will complete this class
+    @Column(nullable = false)
+    private LocalDateTime entryTime;
+
+    private LocalDateTime exitTime;
+
+    private Double fee;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TicketStatus status;
+
+    public enum TicketStatus {
+        ACTIVE,   // vehicle is currently parked
+        CLOSED    // vehicle has exited and fee is paid
+    }
+
+    // Convenience constructor for when a vehicle enters
+    public Ticket(ParkingSlot slot, Vehicle vehicle) {
+        this.slot = slot;
+        this.vehicle = vehicle;
+        this.entryTime = LocalDateTime.now();
+        this.status = TicketStatus.ACTIVE;
+    }
 }
